a5b15352d5e9aef2c0040e3bccf22cff82c5a44e
Alex Golubev <lex_golubev@mail.ru>
2014-03-26 17:36:32 +0300

    First commit

---
+++ pogoda.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+. $PWD/pogoda.conf
+
+while (true)
+do
+	#if [ -f $PAGEFILE ]; then rm $PAGEFILE; fi
+	wget -t 1 -O $PAGEFILE -o wget.log http://www.gismeteo.by/weather-minsk-4248/
+	grep -m 1 "<dd class='value m_temp c'>" $PAGEFILE > temp
+	echo "temperature: " $(sed 's/[^\-\+0-9]//g' temp)
+	rm temp
+	sleep $DELAY
+done
